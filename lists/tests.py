d5318c3798d600209fa2ffef0010c0604edf5d9c
Junyuan Xue <junyuanxue.is@gmail.com>
2016-07-24 18:05:59 +0100

    tests green

--- lists/tests.py
+++ lists/tests.py
@@ -13,6 +13,6 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_url(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(response.content.startsWith(b'<html>'))
+        self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn(b'<title>To-Do Lists</title>', response.content)
-        self.assertTrue(response.content.endsWith(b'</html>'))
+        self.assertTrue(response.content.endswith(b'</html>'))
